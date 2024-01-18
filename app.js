13e7ee7979486e633fc4f89e9f3761faf1d6c0af
Johann <jgcamiloaga@gmail.com>
2024-01-18 17:52:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let numeroSecreto = 6;
+let numeroUsurio = prompt("Me indicas un número, por favor:");
+
+console.log(numeroUsurio);
+
+if (numeroSecreto == numeroUsurio) {
+    alert('Acertaste el número');
+}
+
+
