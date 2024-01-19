e0017305b652f963d7bf14e4707b2fdb181ca811
Johann <jgcamiloaga@gmail.com>
2024-01-18 19:23:30 -0500

    Alura

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-let numeroSecreto = 6;
-let numeroUsurio = prompt("Me indicas un número, por favor:");
+//Estas son las variables
+let numeroSecreto = 2;
+let numeroUsurio = prompt("Me indicas un número entre 1 y 10, por favor:");
 
 console.log(numeroUsurio);
-
-if (numeroSecreto == numeroUsurio) {
-    alert('Acertaste el número');
+/*
+Este código realiza
+la comparación 
+*/
+if (numeroUsurio == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroSecreto}`);
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
 }
-
-
